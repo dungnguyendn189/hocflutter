ffdec8411a0b41202f4a267cc10264fcd7c0dcac
DESKTOP-JGVHL3U\DungHeo <dungnguyendn189>
2022-02-27 21:47:30 +0700

    no message

--- maytinhflutter/calculator/lib/logic_caculator.dart
+++ maytinhflutter/calculator/lib/logic_caculator.dart
@@ -3,8 +3,5 @@ import 'package:rxdart/rxdart.dart';
 
 class Logicalculator {
   BehaviorSubject<String> currentText = BehaviorSubject<String>.seeded('');
-  BehaviorSubject<String> operatorText = BehaviorSubject<String>.seeded('');
   BehaviorSubject<String> first = BehaviorSubject<String>.seeded('');
-  BehaviorSubject<String> second = BehaviorSubject<String>.seeded('');
-  BehaviorSubject<String> result = BehaviorSubject<String>.seeded('');
 }
